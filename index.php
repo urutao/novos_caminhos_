38a37ce48b72f777bcdb4bd48d4495e35f3634a9
IURI ALBUQUERQUE REIS <69777395+iareis@users.noreply.github.com>
2020-08-25 11:49:52 -0300

    Simulação outro usuário
    Intutito testar [ git pull ]

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 // teste para código
 // e envio GiT... add . || status || commit -m "description" || push || pull ||
 ... teste
+// SIMULAÇÃO: de introdução de código por outro usuário, para testar git pull
