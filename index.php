9f61ec0211abbe31929acf997574dc3cb9fa8f4d
Iuri Albuquerque Reis <iareis@gmail.com>
2020-08-25 17:44:37 -0300

    x

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 // teste para código
 // e envio GiT... add . || status || commit -m "description" || push || pull ||
-// ... teste
-// SIMULAÇÃO: de introdução de código por outro usuário, para testar git pull
-// ...
-// ** CORREÇÃO 
+... teste
